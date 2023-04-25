42d8347679c14bda1dab2eddd1d42d04dfaf1c38
Admin <Admin@DESKTOP-9L5JQOO>
2023-04-25 17:46:30 +0800

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+public class Notepad
+{
+    public static void Main()
+    {
+        string fileName = "data.txt"; 
+        string data = "Hello Universe"; 
+
+        
+        WriteDataToFile(fileName, data);
+
+        
+        string readData = ReadDataFromFile(fileName);
+
+        Console.WriteLine("Read data: " + readData);
+    }
+
+    
+    public static void WriteDataToFile(string fileName, string data)
+    {
+        try
+        {
+            
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                
+                writer.Write(data);
+            }
+
+            Console.WriteLine("Data written to the file successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error writing data to the file: " + ex.Message);
+        }
+    }
+
+    
+    public static string ReadDataFromFile(string fileName)
+    {
+        string data = "";
+
+        try
+        {
+            
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                
+                data = reader.ReadToEnd();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error reading data from the file: " + ex.Message);
+        }
+
+        return data;
+    }
+}
